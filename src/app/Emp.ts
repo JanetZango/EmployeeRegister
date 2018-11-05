1bcca6c3d826efc724174dbcd980a5142297bf6e
JanetZango <39265691+JanetZango@users.noreply.github.com>
2018-11-05 14:41:37 +0200

    Add files via upload

---
+++ src/app/Emp.ts
@@ -0,0 +1,14 @@
+export class Employee{
+    identification:string;
+    name:string;
+    surname:string;
+    role:string;
+
+    constructor(identification,name,surname,role){
+    this.identification =identification;
+    this.name = name;
+    this.surname = surname;
+    this.role = role;
+
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { NgModule } from '@angular/core';
 
-
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { EmployeePage } from '../pages/employee/employee';
-import { LoginPage } from '../pages/login/login';
-import { EmployeeDetailsPage } from '../pages/employee-details/employee-details';
-import { RegisterPage } from '../pages/register/register';
-
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage,
-    EmployeePage,
-    LoginPage,
-    EmployeeDetailsPage,
-    RegisterPage,
-
-
- 
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+    BrowserModule
   ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    EmployeePage,
-    LoginPage,
-    EmployeeDetailsPage,
-    RegisterPage,
-  
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
